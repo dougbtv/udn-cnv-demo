a1a3b99989c875641053aa25bf3449aede2a5840
dougbtv <dosmith@redhat.com>
2024-10-24 17:18:47 -0400

    fix kubeconfig location

--- 00-all.sh
+++ 00-all.sh
@@ -2,5 +2,8 @@
 
 > all.log
 OPENSHIFT_RELEASE="${OPENSHIFT_RELEASE:-4.18.0-0.nightly-2024-10-23-112324}"
-export KUBECONFIG="/home/doug/codebase/udn-cnv-demo/openshift-$OPENSHIFT_RELEASE/install/auth/kubeconfig"
-./01-cluster-create.sh && ./02-cnv-setup.sh && ./03-storage-setup.sh && ./04-launch-vm.sh | tee -a all.log
+export KUBECONFIG="$(pwd)/openshift-$OPENSHIFT_RELEASE/install/auth/kubeconfig"
+./01-cluster-create.sh && \
+  ./02-cnv-setup.sh && \
+  ./03-storage-setup.sh && \
+  ./04-launch-vm.sh | tee -a all.log
