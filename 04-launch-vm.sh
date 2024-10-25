446db969a800c6c396b7ee91e42e36637d43614a
dougbtv <dosmith@redhat.com>
2024-10-25 13:02:32 -0400

    uses Miguel's VM launch

--- 04-launch-vm.sh
+++ 04-launch-vm.sh
@@ -6,16 +6,18 @@ log() {
   echo "$(date '+%Y-%m-%d %H:%M:%S') - $message"
 }
 
-log "Creating UDN CR and VM..."
+# This resulted in no change, but...
+oc patch hco -n openshift-cnv kubevirt-hyperconverged --type=json -p='[{"op":"replace","path":"/spec/featureGates/primaryUserDefinedNetworkBinding","value":true},{"op":"replace","path":"/spec/featureGates/deployKubevirtIpamController","value":true}]'
 
-oc project demo
+oc create -f vm-miguel.yml
 
-oc apply -f udn.yml
-
-oc create -f vm.yml
+# log "Creating UDN CR and VM..."
+# oc project demo
+# oc apply -f udn.yml
+# oc create -f vm.yml
 
 log "You can console to your vm with:"
-log "virtctl console fedora-vm"
+log "virtctl console vm-a"
 log "(login with: fedora/fedora)"
 
 # oc get pod virt-launcher-fedora-vm-sz7tr -o jsonpath="{.metadata.annotations['k8s\.ovn\.org/pod-networks']}"
